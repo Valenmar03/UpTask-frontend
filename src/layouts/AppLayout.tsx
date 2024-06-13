8b6d1fc0d49b3aed62936daa61b1d2acd1c88fe8
Valenmar03 <valenmrtnz8@gmail.com>
2024-06-13 10:08:15 -0300

    end header

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -3,42 +3,41 @@ import { Outlet } from "react-router-dom";
 import { SunIcon, MoonIcon } from "@heroicons/react/20/solid";
 
 import Logo from "../components/Logo";
+import NavbarMenu from "../components/NavbarMenu";
 
 export default function AppLayout() {
-    const [theme, setTheme] = useState(() => {
-        if (window.matchMedia("(prefers-color-scheme:dark").matches) {
-           return "dark";
-        }
-        document.querySelector("html")?.classList.add("pretty-bg");
-        return "light";
-     });
-  
-  
-     useEffect(() => {
-        if (theme === "dark") {
-           document.querySelector("html")?.classList.add("dark");
-           document.querySelector("html")?.classList.remove("pretty-bg");
-        } else {
-           document.querySelector("html")?.classList.remove("dark");
-           document.querySelector("html")?.classList.add("pretty-bg");
-        }
-     }, [theme]);
-  
-     const handleChangeTheme = () => {
-        theme === "light" ? setTheme("dark") : setTheme("light");
-     };
+   const [theme, setTheme] = useState(() => {
+      if (window.matchMedia("(prefers-color-scheme:dark").matches) {
+         return "dark";
+      }
+      document.querySelector("html")?.classList.add("pretty-bg");
+      return "light";
+   });
+
+   useEffect(() => {
+      if (theme === "dark") {
+         document.querySelector("html")?.classList.add("dark");
+         document.querySelector("html")?.classList.remove("pretty-bg");
+      } else {
+         document.querySelector("html")?.classList.remove("dark");
+         document.querySelector("html")?.classList.add("pretty-bg");
+      }
+   }, [theme]);
+
+   const handleChangeTheme = () => {
+      theme === "light" ? setTheme("dark") : setTheme("light");
+   };
 
    return (
       <>
-            <header className="bg-zinc-800 py-5">
-                <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
-                    <div className="w-64">
-                        <Logo/>
-                    </div>
+         <header className="bg-neutral-800 py-5 border-b border-b-neutral-700 ">
+            <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
+               <div className="w-32">
+                  <Logo />
+               </div>
 
-                    <nav>
-
-                    {theme === "dark" ? (
+               <div className="flex justify-end gap-7 items-center">
+                  {theme === "dark" ? (
                      <SunIcon
                         className="text-sky-600 size-9 hover:scale-110 p-1 duration-300 cursor-pointer"
                         onClick={handleChangeTheme}
@@ -49,10 +48,19 @@ export default function AppLayout() {
                         onClick={handleChangeTheme}
                      />
                   )}
-                    </nav>
-                </div>
-            </header>
-         <Outlet />
+                  <NavbarMenu />
+               </div>
+            </div>
+         </header>
+         <section className="max-w-screen-2xl mx-auto mt-10 p-5">
+            <Outlet />
+         </section>
+
+         <footer className="py-5">
+            <p className="text-center ">
+               Valentin Martinez {new Date().getFullYear()}
+            </p>
+         </footer>
       </>
    );
 }
