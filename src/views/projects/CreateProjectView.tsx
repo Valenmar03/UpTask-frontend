9e25b489f51d5e5da8dd038927baa1d988adb709
Valenmar03 <valenmrtnz8@gmail.com>
2024-06-14 10:27:44 -0300

    create project and redirect to index page

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,10 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import ProjectForm from "../../components/projects/ProjectForm";
 import { ProjectFormData } from "../../types";
+import { createProject } from "../../api/ProjectAPI";
 
 export default function CreateProjectView() {
-   const initialValues = {
+
+   const navigate = useNavigate()
+   const initialValues : ProjectFormData = {
       projectName: "",
       clientName: "",
       description: "",
@@ -15,8 +18,9 @@ export default function CreateProjectView() {
       formState: { errors },
    } = useForm({ defaultValues: initialValues });
 
-   const handleForm = (data : ProjectFormData) => {
-      console.log(data);
+   const handleForm = async (data : ProjectFormData) => {
+      await createProject(data)
+      navigate('/')
    };
 
    return (
