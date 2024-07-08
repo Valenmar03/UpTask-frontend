ee6e428c1141f86a9d0d7902c56a644eb09ae964
Valenmar03 <valenmrtnz8@gmail.com>
2024-07-08 16:42:38 -0300

    Fromatear y mostrar al fecha de creacion y de actualizacion de una tarea

--- src/components/tasks/DetailsTask.tsx
+++ src/components/tasks/DetailsTask.tsx
@@ -9,35 +9,37 @@ import EditTask from "./EditTask";
 import { Task } from "../../types";
 import { deleteTask } from "../../api/TaskAPI";
 import { toast } from "react-toastify";
+import { formatDate } from "../../utils/utils";
 
 export default function DetailsTask({ data }: { data: Task }) {
    const [editTask, setEditTask] = useState(false);
 
-   const params = useParams()
-   const projectId = params.projectId!
-   const queryClient = useQueryClient()
-   const navigate = useNavigate()
-   
+   const params = useParams();
+   const projectId = params.projectId!;
+   const queryClient = useQueryClient();
+   const navigate = useNavigate();
+
    const { mutate } = useMutation({
       mutationFn: deleteTask,
       onError: () => {
-         toast.error("Error eliminando tarea")
+         toast.error("Error eliminando tarea");
       },
       onSuccess: () => {
-         toast.success("Tarea Eliminada Correctamente")
-         queryClient.invalidateQueries({ queryKey: ["editProject", projectId]})
-         navigate("", {replace: true})
-      }
-   })
+         toast.success("Tarea Eliminada Correctamente");
+         queryClient.invalidateQueries({
+            queryKey: ["editProject", projectId],
+         });
+         navigate("", { replace: true });
+      },
+   });
 
    const handleDelete = () => {
       const deleteData = {
          taskId: data._id,
-         projectId
-      }
-      mutate(deleteData)
-   }
-   
+         projectId,
+      };
+      mutate(deleteData);
+   };
 
    return (
       <>
@@ -45,16 +47,16 @@ export default function DetailsTask({ data }: { data: Task }) {
             <EditTask data={data} setEditTask={setEditTask} />
          ) : (
             <>
+               
                <div className="flex flex-col sm:flex-row sm:justify-between group sm:items-center mb-2 mt-5">
-                  <h2 className="text-4xl font-bold px-10">
-                     {data.name}
-                  </h2>
+                  <h2 className="text-4xl font-bold px-10">{data.name}</h2>
                   <div className="flex items-center space-x-3 mx-2 px-10">
                      <PencilIcon
                         className="size-7 hover:scale-110  duration-150 text-purple-600 cursor-pointer"
                         onClick={() => setEditTask(true)}
                      />
-                     <TrashIcon className="size-7 hover:scale-110 duration-150 text-red-600 cursor-pointer" 
+                     <TrashIcon
+                        className="size-7 hover:scale-110 duration-150 text-red-600 cursor-pointer"
                         onClick={handleDelete}
                      />
                   </div>
@@ -65,6 +67,10 @@ export default function DetailsTask({ data }: { data: Task }) {
                      Descripcion
                   </h3>
                   <p>{data.description}</p>
+               </div>
+               <div className="px-10 text-xs mt-5 opacity-40">
+                  <p>Creado el: <span className="font-bold">{formatDate(data.createdAt)}</span></p>
+                  <p>Ultima actualización el: <span className="font-bold">{formatDate(data.updatedAt)}</span></p>
                </div>
             </>
          )}
