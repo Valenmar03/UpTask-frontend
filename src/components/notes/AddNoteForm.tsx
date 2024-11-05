5a8d65791243659cfb224e49974cbf2d25cbddfb
Valenmar03 <valenmrtnz8@gmail.com>
2024-11-05 12:39:30 -0300

    Le doy funcionalidad al formulario con useForm de react-hook-form

--- src/components/notes/AddNoteForm.tsx
+++ src/components/notes/AddNoteForm.tsx
@@ -1,19 +1,43 @@
+import { useForm } from "react-hook-form";
+import { NoteFormData } from "../../types";
 
 export default function AddNoteForm() {
-  return (
-    <form
-        onSubmit={() => {}}
-        className="space-y-3"
-        noValidate
-    >
-        <div className="grid gap-1 grid-cols-10 w-full">
-            <input id="content" type="text" className="bg-gray-200 dark:bg-neutral-700 rounded-md text-xl py-2 pl-2 col-span-8"/>
-            <input 
-                type="submit" 
-                value="+" 
-                className="bg-blue-200 hover:bg-blue-300 dark:bg-neutral-900 dark:hover:bg-neutral-950 text-3xl rounded-md cursor-pointer  col-span-2 duration-200 text-center text-blue-700 dark:text-gray-200 w-full"
+   const initialValues: NoteFormData = {
+      content: "",
+   };
+
+   const {
+      register,
+      handleSubmit,
+      formState: { errors },
+   } = useForm({ defaultValues: initialValues });
+
+   const handleAddNote = (formData: NoteFormData) => {
+    
+   }
+
+   return (
+      <form onSubmit={handleSubmit(handleAddNote)} noValidate>
+         <div className="grid gap-1 grid-cols-10 w-full">
+            <input
+               id="content"
+               type="text"
+               className="bg-gray-200 dark:bg-neutral-700 rounded-md text-xl py-2 pl-2 col-span-8"
+               {...register("content", {
+                  required: "La nota debe tener contenido",
+               })}
             />
-        </div>
-    </form>
-  )
+            <input
+               type="submit"
+               value="+"
+               className="bg-blue-200 hover:bg-blue-300 dark:bg-neutral-900 dark:hover:bg-neutral-950 text-3xl rounded-md cursor-pointer  col-span-2 duration-200 text-center text-blue-700 dark:text-gray-200 w-full"
+            />
+         </div>
+         {errors.content && (
+            <p className="text-sm text-red-400 dark:text-red-700">
+               {errors.content.message}
+            </p>
+         )}
+      </form>
+   );
 }
