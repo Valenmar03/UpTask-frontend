f7bb6d2d8113ebe74782bd50210b055e9116da67
Valenmar03 <valenmrtnz8@gmail.com>
2024-11-07 14:26:02 -0300

    Eliminar las notas

--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -3,10 +3,35 @@ import { Note, Project } from "../../types";
 import { formatDate } from "../../utils/utils";
 import { useAuth } from "../../hooks/useAuth";
 import { isManager } from "../../utils/policies";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useLocation, useParams } from "react-router-dom";
+import { deleteNote } from "../../api/NoteAPI";
+import { toast } from "react-toastify";
 
 export default function NoteDetail({ note, projectManager }: { note: Note, projectManager: Project['manager'] }) {
 
    const { data:  user } = useAuth()
+
+   const params = useParams()
+   const location = useLocation()
+   const queryParams = new URLSearchParams(location.search)
+   const queryClient = useQueryClient() 
+
+   const projectId = params.projectId!
+   const taskId = queryParams.get('taskId')!
+   const { mutate } = useMutation({
+    mutationFn: deleteNote,
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ['task', taskId]})
+    }
+   })
+
+   const handleDeleteNote = () => {
+      mutate({projectId, taskId, noteId: note._id})
+   }
 
    return (
       <div className="bg-gray-200 dark:bg-neutral-800 rounded-md py-2 pl-2 flex items-center justify-between group ">
@@ -20,7 +45,7 @@ export default function NoteDetail({ note, projectManager }: { note: Note, proje
             (user!.name === note.createdBy.name || isManager(projectManager, user!._id)) &&
             <TrashIcon 
                className="size-8 cursor-pointer opacity-0 group-hover:opacity-100 text-red-700 pr-2"
-               onClick={() => {}}
+               onClick={handleDeleteNote}
             />
          }
       </div>
